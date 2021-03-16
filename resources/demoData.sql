ecea8ba7640469360c2157a6e0465303b1657d82
Markus Winter <markus.winter@students.fhnw.ch>
2021-03-16 10:32:59 +0100

    "SQL demodata" and "Postman Collection"

---
+++ resources/demoData.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.incidence ("bfsNr", date, incidence) VALUES (3522, '2021-01-01', 9.6);
+INSERT INTO public.incidence ("bfsNr", date, incidence) VALUES (3522, '2021-02-01', 11.5);
+INSERT INTO public.incidence ("bfsNr", date, incidence) VALUES (3522, '2021-03-01', 13.9);
+INSERT INTO public.incidence ("bfsNr", date, incidence) VALUES (3561, '2021-01-01', 4.3);
+INSERT INTO public.incidence ("bfsNr", date, incidence) VALUES (3561, '2021-02-01', 4.7);
+
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3522, 7477, 'Filisur', 'GR', 466, 7000, '4449');
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3531, 7457, 'Bivio', 'GR', 204, 7673, 'Albula');
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3539, 7460, 'Savognin', 'GR', 882, 2224, 'Albula');
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3551, 7743, 'Brusio', 'GR', 1202, 4629, 'Bernina');
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3551, 7744, 'Brusio', 'GR', 1202, 4629, 'Bernina');
+INSERT INTO public.municipality ("bfsNr", "zipCode", name, canton, area, population, region) VALUES (3561, 7710, 'Poschiavo', 'GR', 3225, 19101, 'Bernina');
