20e83121471bccccb39874c3e3ded6029ba3b1b0
Marco Peter <marco-peter@bluewin.ch>
2021-03-29 19:01:08 +0200

    Prepare folder structure

---
+++ fetch_incidence.py
@@ -0,0 +1,6 @@
+# Entry point for incidence fetching
+# Used for initial incidence fetching
+# Used for incremental incidence fetching
+
+# Entry point will be called via CLI with arguments
+# https://docs.python.org/3/library/argparse.html
---
+++ fetch_municipality.py
@@ -0,0 +1,5 @@
+# Entry point for municipality fetching
+# Used for initial municipality fetching
+
+# Entry point will be called via CLI with arguments
+# https://docs.python.org/3/library/argparse.html
