c6def391add0000edc15ae0025f4d39ab7d67686
Markus Winter <markus.winter@students.fhnw.ch>
2021-04-05 03:12:43 +0200

    Add Applicaton Prefix

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from configManager import ConfigManager
 from controllers.incidenceController import incidence_controller
 from controllers.municipalityController import municipality_controller
+from controllers.swaggerUIController import swaggerui_controller
 from setup import app
 
-app.register_blueprint(incidence_controller)
-app.register_blueprint(municipality_controller)
+server_config = ConfigManager.get_instance().get_server_config()
+application_root = ConfigManager.get_instance().get_application_root()
+
+app.register_blueprint(incidence_controller, url_prefix=application_root)
+app.register_blueprint(municipality_controller, url_prefix=application_root)
+
 
 if __name__ == '__main__':
-    server_config = ConfigManager.get_instance().get_server_config()
-
     app.config['DEVELOPMENT'] = server_config["development"]
     app.run(debug=server_config["debug"], host=server_config["host"], port=server_config["port"])
